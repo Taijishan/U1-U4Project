c7e9f0f094cc30896019f69bc22120a2ba036843
unknown <BT_4N2_09@BKTECH.LOCAL>
2023-11-06 14:03:07 -0500

    Initial commit

---
+++ src/NovelRecommendation.java
@@ -0,0 +1,12 @@
+public class NovelRecommendation {
+    private String name;
+    private String language;
+public NovelRecommendation(String novelName, String novelLanguage)
+{
+    name = novelName;
+    language = novelLanguage;
+}
+String [] cNovels = {"Archfiend", "RSSG", "LOTM", "The Legendary Mechanic"};
+String [] kNovels = {"The Remarried Empress"};
+
+}
---
+++ src/NovelRecommendationRunner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NovelRecommendationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to the novel recommendation program!");
+        System.out.println("You enter information of your favorite novel, and I shall provide a recommendation based on the information provided.");
+        System.out.println("Please enter the name of the Novel(no acronyms): ");
+        String userNovel = s.nextLine();
+        System.out.println("Please enter the original language of the novel: ");
+        String userLanguage = s.nextLine();
+        NovelRecommendation recommend = new NovelRecommendation(userNovel, userLanguage);
+        System.out.println(recommend);
+    }
+
+}
