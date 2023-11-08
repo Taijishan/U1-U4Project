003064bf252adca64b287c460b72262045c4473b
joever <a@a>
2023-11-08 14:05:13 -0500

    update day 1

--- src/NovelRecommendation.java
+++ src/NovelRecommendation.java
@@ -1,12 +1,33 @@
+import java.util.ArrayList;
+
 public class NovelRecommendation {
     private String name;
     private String language;
-public NovelRecommendation(String novelName, String novelLanguage)
-{
+    public NovelRecommendation(String novelName, String novelLanguage)
+    {
     name = novelName;
     language = novelLanguage;
-}
-String [] cNovels = {"Archfiend", "RSSG", "LOTM", "The Legendary Mechanic"};
-String [] kNovels = {"The Remarried Empress"};
+    }
+    String [] cNovels = {"Archfiend", "RSSG", "LOTM", "The Legendary Mechanic"};
+    String [] kNovels = {"The Remarried Empress", "Clearing the game at the End of the World"};
+    String [] longTitle = {"fdhtrhtt5ggregreggrgregreg"};
+    int randomCNovel = (int) (Math.random() * cNovels.length);
+    int randomKNovel = (int) (Math.random() * kNovels.length);
+    int randomLongNovel = (int) (Math.random() * longTitle.length);
 
+    public String getNovelFromWords(){
+        if (name.length() > 50){
+            String bigNovel = "recommend Novel based on title inputted: " + longTitle[randomLongNovel];
+            return bigNovel;
+        }
+    }
+    public String getNovelFromLanguage() {
+        if (language.equals("chinese")) {
+            String chineseRec = "recommend Novel based on language inputted: " + cNovels[randomCNovel];
+            return chineseRec;
+        } else {
+            String koreanRec = "recommend novel based on language inputted: " + kNovels[randomKNovel];
+            return koreanRec;
+        }
+    }
 }
--- src/NovelRecommendationRunner.java
+++ src/NovelRecommendationRunner.java
@@ -6,10 +6,12 @@ public class NovelRecommendationRunner {
         System.out.println("You enter information of your favorite novel, and I shall provide a recommendation based on the information provided.");
         System.out.println("Please enter the name of the Novel(no acronyms): ");
         String userNovel = s.nextLine();
-        System.out.println("Please enter the original language of the novel: ");
+        System.out.println("Please enter the original language of the novel(Chinese/Korean): ");
         String userLanguage = s.nextLine();
+        userLanguage = userLanguage.toLowerCase();
         NovelRecommendation recommend = new NovelRecommendation(userNovel, userLanguage);
-        System.out.println(recommend);
+        System.out.println(recommend.getNovelFromLanguage());
+        System.out.println(recommend.getNovelFromWords());
     }
 
 }
